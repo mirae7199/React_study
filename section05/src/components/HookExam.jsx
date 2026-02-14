470a1e1d5740ca0495d21b08a6d461c4db2e76b6
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-02-14 23:48:04 +0900

    feat: Hooks

---
+++ section05/src/components/HookExam.jsx
@@ -0,0 +1,40 @@
+import useInput from "./../hooks/useInput";
+
+
+// 3가지 hook 관련된 팁
+// 1. 함수 컴포넌트, 커스텀 훅 내부에서만 호출 가능
+// const state = useState(); X
+
+// 2. 조건부로 호출될 수는  없다.
+// 3. 나만의 훅(Custom Hook)를 직접 만들 수 있다.
+
+// function useInput() { // 커스텀 훅으로 만들려면 use 라는 접두사를 붙이면 됨.
+//     const [input, setInput] = useState("");
+
+//     const onChange = (e)=> {
+//         setInput(e.target.value);
+//         console.log(input);
+//     };
+
+//     return [input, onChange];
+
+// }
+
+ const HookExam = ()=> {
+
+    const [input, onChange] = useInput();
+    const [input2, onChange2] = useInput();
+
+    // if (true) {
+    //     const state = useState();
+    // }
+
+    return (
+    <div>
+        <input value={input} onChange={onChange} />
+        <input value={input2} onChange={onChange2} />
+    </div>
+    );
+};
+
+export default HookExam;
